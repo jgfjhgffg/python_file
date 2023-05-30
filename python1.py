aca6764719d947dd58f07bc86a52278ef5268c0b
jgfjhgffg <135002256+jgfjhgffg@users.noreply.github.com>
2023-05-30 16:59:45 +1000

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,12 @@
+print("list")
+ll=[1,2,"ABC",3,"xyz",2.3]
+print(ll)
+print("dictionary")
+d1={"a":134,"b":266,"c":343}
+print(d1)
+print("tuples")
+t1=(10,20,30,40,50,60,70,80,90)
+print(t1)
+print("sets")
+s1={10,30,20,40,10,30,40,20,50,50}
+print(s1)
